ec90f5ad401501f957f1906088ed6a0cb27dd9b8
Lautaro <lautaro.scavuzzo0@gmail.com>
2021-08-30 14:00:06 -0300

    db.js updated

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-const conexion = process.env.MONGOURL || "mongodb+srv://lauti:1234@proyectolibros.jbsm0.mongodb.net/test";
+const conexion = "mongodb+srv://lauti:1234@proyectolibros.jbsm0.mongodb.net/test";
 mongoose.connect(conexion, { useNewUrlParser: true, useUnifiedTopology: true });
