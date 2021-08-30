ca29c59abea1202f202dace1399a99c60c144d4d
Lautaro <lautaro.scavuzzo0@gmail.com>
2021-08-30 14:40:06 -0300

    database updated

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-const conexion = "mongodb+srv://lauti:1234@proyectolibros.jbsm0.mongodb.net/test";
+const conexion = "mongodb+srv://lauti:1234@proyectolibros.jbsm0.mongodb.net/proyectolibros?retryWrites=true&w=majority";
 mongoose.connect(conexion, { useNewUrlParser: true, useUnifiedTopology: true });
--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 // Importamos la conexión a la base de datos
 var db = require('./db');
-const port = process.env.PORT || 8181
+const port = process.env.PORT || 8181;
 
 // El archivo que tiene todo el armado de la Rest API
 var librosController = require('./libros/librosController');
+
+app.get('/' , (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
 
 // La Rest API queda en /api/
 app.use('/api', librosController);
